e6525ffbe350f1260f62e2d5e263131d03fe5974
robzdoom <jorgecruz.rd@gmail.com>
2017-05-30 11:17:29 -0700

    added action and reducer. API is being called

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_POST = 'fetch_posts'
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=PaperWilly1234'
 
-export function fetchPost(){
+export function fetchPosts(){
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
     
     return {
