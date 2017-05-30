2c70c0e4ba0ff8d5a07118989a10e6fabe90a7b1
robzdoom <jorgecruz.rd@gmail.com>
2017-05-30 14:06:23 -0700

    rendering title for blog

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostIndex extends Component {
     componentDidMount(){
         this.props.fetchPosts();
     }
+    
+    renderPosts(){
+       return _.map(this.props.posts, post => {
+           return (
+           <li className='list-group-item' key={post.id}>
+               {post.title}
+           </li>
+           );
+       })
+    }
+
     render(){
-        console.log(this.props.posts)
         return (
-            <div>Post Index</div>
+            <div>
+                <h3> Post </h3>
+                <ul className = 'list-group'> 
+                    {this.renderPosts()}
+                </ul>
+            </div>
         )
     }
 }
