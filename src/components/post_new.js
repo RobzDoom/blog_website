43861ff0d1ec0d1729fac6ee3a84e05ee514915a
robzdoom <jorgecruz.rd@gmail.com>
2017-05-31 11:23:27 -0700

    created render method

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
+//reduxForm is very similar to connect
+
 class PostNew extends Component {
+    renderTitleField(field){
+        return (
+            <div>
+                <input 
+                    type = "text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+    
     render(){
         return(
             <form> 
                 <Field 
                     name = "title"
-                    component = {}
+                    component = {this.renderTitleField}
                 />
             </form>
         )
