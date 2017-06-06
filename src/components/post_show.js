9a7f88702c0a02dc08db57922685186793dcbc4a
robzdoom <jorgecruz.rd@gmail.com>
2017-06-06 12:20:11 -0700

    added delete blog

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions'
+import { fetchPost, deletePost } from '../actions'
 import { Link } from 'react-router-dom'
+
 
 
 class PostsShow extends Component {
     componentDidMount (){
        const { id } =  this.props.match.params
         this.props.fetchPost(id);
+    }
+    removeBlog(){
+        const { id } =  this.props.match.params
+        this.props.deletePost(id, () => {
+            this.props.history.push('/')
+        });
     }
     render(){
         const { post } = this.props
@@ -19,6 +26,11 @@ class PostsShow extends Component {
         return (
             <div>
                 <Link to='/'>Back to Main Page </Link>
+                 <button className='btn btn-danger pull-xs-right'
+                        onClick={this.removeBlog.bind(this)}
+                 >
+                    Delete Post
+                 </button>
                 <h3> { post.title }</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p> {post.content} </p>
@@ -32,4 +44,4 @@ function mapStateToProps({ posts }, ownProps){
     return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchPost }) (PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost }) (PostsShow);
